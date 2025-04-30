5c91e6cd91eb4ffdbf054d0c53428714a8fc86c8
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-04-30 17:32:11 +0530

    Writting in File

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt', 'UTF-8');
 console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written');
